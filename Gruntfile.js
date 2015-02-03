0b1b6a56bb581118432ed43d447be307074aed8b
Patrik Falk <patrik@patrikfalk.com>
2015-02-03 19:39:00 +0100

    Updated the package.json to use gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  var config = {
+    pkg: grunt.file.readJSON('package.json'),
+    staticHandlebars: {
+      simpleTarget: {
+        files:{'dist/*.html':'src/*.hbs'}
+      }
+    },
+    watch: {
+      src: {
+        files: ['src/*'],
+        tasks: ['staticHandlebars'],
+        options: {
+          spawn: false,
+        },
+      }
+    }
+  };
+
+  grunt.initConfig(config);
+  grunt.loadNpmTasks('grunt-static-handlebars');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+};
